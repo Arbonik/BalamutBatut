0f412eaa86b4e4d8dc376902b80d17611df2ba50
arbonik <zmear@mail.ru>
2021-01-10 17:21:22 +0700

    add GroupViewModel.kt Fragment

--- app/src/main/java/com/castprogramms/balamutbatut/MainActivity.kt
+++ app/src/main/java/com/castprogramms/balamutbatut/MainActivity.kt
@@ -2,10 +2,12 @@ package com.castprogramms.balamutbatut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
---
+++ app/src/main/java/com/castprogramms/balamutbatut/ui/group/GroupViewModel.kt
@@ -0,0 +1,7 @@
+package com.castprogramms.balamutbatut.ui.group
+
+import androidx.lifecycle.ViewModel
+
+class GroupViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
