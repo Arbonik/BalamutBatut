d17a59271d650762438c53673cfc95c66d3ffdbf
kingofraccoons <zvezdakov2015@yandex.ru>
2021-01-26 18:05:29 +0700

    registr Google fix

--- app/src/main/java/com/castprogramms/balamutbatut/ui/registr/RegistrFragment.kt
+++ app/src/main/java/com/castprogramms/balamutbatut/ui/registr/RegistrFragment.kt
@@ -5,21 +5,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.castprogramms.balamutbatut.R
 import com.castprogramms.balamutbatut.tools.DataUserFirebase
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 
 class RegistrFragment: Fragment() {
     lateinit var registrViewModel: RegistrViewModel
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,10 +27,15 @@ class RegistrFragment: Fragment() {
         registrViewModel = RegistrViewModel()
         val view = inflater.inflate(R.layout.fragment_regist_google, container, false)
         registrViewModel.initGoogleSign(requireContext())
-
+        val button : SignInButton = view.findViewById(R.id.sign_in_button)
+        button.setOnClickListener {
+            signIn()
+        }
         return view
     }
-
+    fun signIn(){
+        startActivityForResult(Intent(registrViewModel.googleSignInClient.signInIntent), registrViewModel.SIGN_IN_CODE)
+    }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == registrViewModel.SIGN_IN_CODE){
