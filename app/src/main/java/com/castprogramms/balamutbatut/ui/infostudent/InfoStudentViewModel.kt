81455aa9a5bfcd6e9fd746ab6228bc221626540f
arbonik <zmear@mail.ru>
2021-04-04 17:10:14 +0700

    add koin

--- app/src/main/java/com/castprogramms/balamutbatut/ui/infostudent/InfoStudentViewModel.kt
+++ app/src/main/java/com/castprogramms/balamutbatut/ui/infostudent/InfoStudentViewModel.kt
@@ -3,18 +3,24 @@ package com.castprogramms.balamutbatut.ui.infostudent
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.castprogramms.balamutbatut.Repository
+import com.castprogramms.balamutbatut.tools.Element
 import com.castprogramms.balamutbatut.users.Student
 
 class InfoStudentViewModel(private val repository : Repository) : ViewModel() {
 
     val mutableLiveDataStudent = MutableLiveData<Student>()
+    var mutableLiveElements = MutableLiveData<MutableList<Element>>()
 
-    fun loadData(idStudent : String){
+    fun loadUserData(idStudent : String){
         repository.getStudent(idStudent).addSnapshotListener { value, error ->
             if (value != null){
                 mutableLiveDataStudent.value = value.toObject(Student::class.java)
             }
         }
+    }
+
+    fun loadElementsData(elements : List<Element>){
+        mutableLiveElements = repository.getElement(elements)
     }
 
 }
