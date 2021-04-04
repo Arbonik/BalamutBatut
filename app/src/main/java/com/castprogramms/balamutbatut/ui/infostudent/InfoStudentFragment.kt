1e9bc9b5636956c2ef663fd9467f0ea7aca31bb9
kingofraccoons <zvezdakov2015@yandex.ru>
2021-04-04 17:30:12 +0700

    fix update data in trainer

--- app/src/main/java/com/castprogramms/balamutbatut/ui/infostudent/InfoStudentFragment.kt
+++ app/src/main/java/com/castprogramms/balamutbatut/ui/infostudent/InfoStudentFragment.kt
@@ -29,7 +29,7 @@ class InfoStudentFragment: Fragment() {
         if (arguments != null){
             idStudent = arguments?.getString("id", "").toString()
             if (idStudent != "null" && idStudent != ""){
-                viewModel.loadData(idStudent)
+                viewModel.loadUserData(idStudent)
             }
         }
     }
