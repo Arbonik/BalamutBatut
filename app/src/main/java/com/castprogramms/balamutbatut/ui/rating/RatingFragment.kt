3490bde8e251730ea8b851a695600e92c2226eed
polina1441 <14042003dp@mail.ru>
2021-04-06 23:11:06 +0700

    expandable list

--- app/src/main/java/com/castprogramms/balamutbatut/ui/rating/RatingFragment.kt
+++ app/src/main/java/com/castprogramms/balamutbatut/ui/rating/RatingFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListAdapter
+import android.widget.ExpandableListView
 import androidx.fragment.app.Fragment
 import com.castprogramms.balamutbatut.R
 import com.castprogramms.balamutbatut.databinding.FragmentRatingBinding
@@ -11,6 +13,27 @@ import org.koin.android.viewmodel.ext.android.viewModel
 
 class RatingFragment: Fragment() {
     val viewModel : RatingViewModel by viewModel()
+
+    internal var adapter: ExpandableListAdapter?= null
+    internal var titleList: List<String> ?= null
+
+    val easy = "EasyElement"
+    val midlle = "MiddleElement"
+    val hard = "HardElement"
+    val dataMap: HashMap<String, List<String>>
+    get() {
+        val listData = HashMap<String, List<String>>()
+
+        val item = ArrayList<String>()
+        item.add("Легкие: $easy")
+        item.add("Средние: $midlle")
+        item.add("Сложные: $hard")
+
+        listData["Выполненные элементы"] = item
+
+        return dataMap
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,6 +44,20 @@ class RatingFragment: Fragment() {
         val ratingAdapter = RatingAdapter(viewModel.getAllStudent())
         binding.recyclerRating.adapter = ratingAdapter
 
+        val expandableListView = view.findViewById<ExpandableListView>(R.id.expandableListView)
+        if (expandableListView != null) {
+            val listData = dataMap
+            titleList = ArrayList(listData.keys)
+            adapter = ExpandableList(requireContext(), titleList as ArrayList<String>, listData)
+            expandableListView.setAdapter(adapter)
+            expandableListView.setOnGroupExpandListener { groupPosition ->  }
+
+            expandableListView.setOnGroupCollapseListener { groupPosition ->  }
+
+            expandableListView.setOnChildClickListener { parent, v, groupPosition, childPosition, id ->
+                false
+            }
+        }
         return view
     }
 }
