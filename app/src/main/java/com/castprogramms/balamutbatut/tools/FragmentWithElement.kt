112415c921c9d1ac238b8ba4eaff43774f3de51d
KingOfRaccoons <zvezdakov2105@yandex.ru>
2021-05-27 10:42:42 +0700

    fix work with all elements

--- app/src/main/java/com/castprogramms/balamutbatut/tools/FragmentWithElement.kt
+++ app/src/main/java/com/castprogramms/balamutbatut/tools/FragmentWithElement.kt
@@ -7,19 +7,40 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.observe
 import com.castprogramms.balamutbatut.Repository
+import com.castprogramms.balamutbatut.network.Resource
 import org.koin.android.ext.android.inject
 
 open class FragmentWithElement(layoutID: Int): Fragment(layoutID) {
     val repository : Repository by inject()
     var mutableLiveData = MutableLiveData<MutableMap<String, List<Element>>>()
+    val mutableLiveDataOrder = MutableLiveData<Resource<List<String>>>()
     open fun generateAdapter(map: Map<String, List<Int>>){
         val maps = mutableMapOf<String, List<Element>>()
-        map.forEach {
-            repository.getElement(it.value.toList(), it.key).observe(viewLifecycleOwner, Observer{ it1 ->
-                maps.put(it.key, it1)
-                Log.e("data", maps.toString())
-                mutableLiveData.postValue(maps)
+        if (map.isNotEmpty()) {
+            map.forEach {
+                repository.getElement(it.value.toList(), it.key)
+                    .observe(viewLifecycleOwner, Observer { it1 ->
+                        maps.put(it.key, it1)
+                        Log.e("data", maps.toString())
+                        mutableLiveData.postValue(maps)
+                    })
+            }
+        }else{
+            repository.getAllElements(mapOf()).observe(viewLifecycleOwner, Observer{
+                mutableLiveData.postValue(it.toMutableMap())
             })
         }
+    }
+
+    open fun getTrueOrder(){
+        repository.getTrueOrder().observe(viewLifecycleOwner, Observer{
+            when(it){
+                is Resource.Error -> {}
+                is Resource.Loading -> {}
+                is Resource.Success -> {
+                    mutableLiveDataOrder.postValue(it)
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/castprogramms/balamutbatut/ui/allElements/AllElementListFragment.kt
+++ app/src/main/java/com/castprogramms/balamutbatut/ui/allElements/AllElementListFragment.kt
@@ -7,37 +7,29 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.castprogramms.balamutbatut.R
 import com.castprogramms.balamutbatut.databinding.AllElementListFragmentBinding
+import com.castprogramms.balamutbatut.network.Resource
+import com.castprogramms.balamutbatut.tools.FragmentWithElement
+import com.castprogramms.balamutbatut.ui.changeprogram.adapters.IHopeThisAdapterCanWork
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class AllElementListFragment : Fragment() {
+class AllElementListFragment : FragmentWithElement(R.layout.all_element_list_fragment) {
     val viewModel: AllElementListViewModel by viewModel()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.all_element_list_fragment, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val binding = AllElementListFragmentBinding.bind(view)
-
-        viewModel.mutableLiveElements.observe(viewLifecycleOwner, Observer {
-            if (it != null) {
-                generateAdapter(it).observe(viewLifecycleOwner) { it1 ->
-                    binding.recyclerAllElementsList.adapter = AdapterAllElements(it1)
-                }
-            }
+        val adapter =  IHopeThisAdapterCanWork(true)
+        binding.recyclerAllElementsList.adapter = adapter
+        generateAdapter(mapOf())
+        getTrueOrder()
+        mutableLiveDataOrder.observe(viewLifecycleOwner, {
+            if (it is Resource.Success)
+                adapter.filters = it.data!!
         })
-
-        return view
-    }
-    fun generateAdapter(map: MutableList<Int>): MutableLiveData<MutableList<Int>> {
-        val maps = mutableMapOf<String, List<Int>>()
-        val mutableMap = MutableLiveData(map)
-        viewModel.getAllElement(maps).observe(viewLifecycleOwner) { it1 ->
-            maps.putAll(it1 as Map<out String, List<Int>>)
-            mutableMap.postValue(mutableMap.value)
-        }
-        return mutableMap
+        mutableLiveData.observe(viewLifecycleOwner, {
+            adapter.setElement(it)
+        })
     }
 }
